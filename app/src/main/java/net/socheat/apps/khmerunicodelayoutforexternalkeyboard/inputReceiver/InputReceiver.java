ac3d4d14ffde543c9d651cb6c2f91f6038d4fa79
Socheat Sok <socheatsok78@gmail.com>
2017-05-25 11:20:17 +0700

    Build Keyboard

---
+++ app/src/main/java/net/socheat/apps/khmerunicodelayoutforexternalkeyboard/inputReceiver/InputReceiver.java
@@ -0,0 +1,16 @@
+package net.socheat.apps.khmerunicodelayoutforexternalkeyboard;
+
+/**
+ * Created by socheat on 5/25/17.
+ */
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class InputReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Nothing to do at this time.
+    }
+}
